f56c50f7321c4835407158d763abce7694974e2c
Jacques Gagnon <darthcloud@gmail.com>
2022-10-21 18:29:30 +0000

    Add GameId support to presets page

--- presets.js
+++ presets.js
@@ -3,16 +3,24 @@ import { getLatestRelease } from './utils/getLatestRelease.js';
 import { getAppVersion } from './utils/getAppVersion.js';
 import { getBdAddr } from './utils/getBdAddr.js';
 import { savePresetInput } from './utils/savePresetInput.js';
+import { getGameId } from './utils/getGameId.js';
+import { getGameName } from './utils/getGameName.js';
+import { getCfgSrc } from './utils/getCfgSrc.js';
+import { setDefaultCfg } from './utils/setDefaultCfg.js';
+import { setGameIdCfg } from './utils/setGameIdCfg.js';
 
 var presets = new Array();
 var bluetoothDevice;
 let brService = null;
 var pageInit = 0;
 var consoles = []
-var bdaddr;
-var app_ver;
-var latest_ver;
-var name;
+var bdaddr = '';
+var app_ver = '';
+var latest_ver = '';
+var name = '';
+var gameid = '';
+var gamename = '';
+var current_cfg = 0;
 
 function initInputSelect() {
     //push all console names from JSON files
@@ -163,7 +171,63 @@ function onDisconnected() {
     log('> Bluetooth Device disconnected');
     document.getElementById("divBtConn").style.display = 'block';
     document.getElementById("divInfo").style.display = 'none';
+    document.getElementById("divCfgSel").style.display = 'none';
     document.getElementById("divInputCfg").style.display = 'none';
+}
+
+function swGameIdCfg() {
+    setGameIdCfg(brService)
+    .then(_ => {
+        return getCfgSrc(brService);
+    })
+    .then(value => {
+        current_cfg = value;
+        initCfgSelection();
+    })
+}
+
+function swDefaultCfg() {
+    setDefaultCfg(brService)
+    .then(_ => {
+        return getCfgSrc(brService);
+    })
+    .then(value => {
+        current_cfg = value;
+        initCfgSelection();
+    })
+}
+
+function initCfgSelection() {
+    let divCfgSel = document.getElementById("divCfgSel");
+    let cfgSw = document.createElement("div");
+    let cfgBtn = document.createElement("button");
+
+    divCfgSel.innerHTML = '';
+
+    let header = document.createElement("h2");
+    header.style.margin = 0;
+    header.innerText = 'Config Selection';
+
+    divCfgSel.appendChild(header);
+
+    cfgBtn.id = "cfgSw";
+
+    if (current_cfg == 0) {
+        cfgBtn.innerText += 'Switch to GameID';
+        cfgBtn.addEventListener("click", swGameIdCfg);
+        divCfgSel.innerHTML += 'Current config: Global';
+        if (gameid.length) {
+            cfgSw.appendChild(cfgBtn);
+        }
+    }
+    else {
+        cfgBtn.innerText = 'Switch to Global';
+        cfgBtn.addEventListener("click", swDefaultCfg);
+        divCfgSel.innerHTML += 'Current config: GameID';
+        cfgSw.appendChild(cfgBtn);
+    }
+    cfgSw.setAttribute("style", "margin-top:1em;");
+    divCfgSel.append(cfgSw);
 }
 
 export function btConn() {
@@ -200,13 +264,35 @@ export function btConn() {
     })
     .then(value => {
         app_ver = value;
-        document.getElementById("divInfo").innerHTML = 'Connected to: ' + name + ' (' + bdaddr + ') [' + app_ver + ']';
+        return getGameId(brService);
+    })
+    .then(value => {
+        gameid = value;
+        return getGameName(gameid);
+    })
+    .then(value => {
+        gamename = value;
+        return getCfgSrc(brService);
+    })
+    .catch(error => {
+        if (error.name == 'NotFoundError'
+          || error.name == 'NotSupportedError') {
+            return 0;
+        }
+        throw error;
+    })
+    .then(value => {
+        current_cfg = value;
+        document.getElementById("divInfo").innerHTML = 'Connected to: ' + name + ' (' + bdaddr + ') [' + app_ver
+            + ']<br> Current Game: ' + gamename + ' (' + gameid + ')';
         if (app_ver.indexOf(latest_ver) == -1) {
             document.getElementById("divInfo").innerHTML += '<br><br>Download latest FW ' + latest_ver + ' from <a href=\'https://darthcloud.itch.io/blueretro\'>itch.io</a>';
         }
         document.getElementById("divBtConn").style.display = 'none';
         document.getElementById("divInfo").style.display = 'block';
+        document.getElementById("divCfgSel").style.display = 'block';
         document.getElementById("divInputCfg").style.display = 'block';
+        initCfgSelection();
     })
     .catch(error => {
         log('Argh! ' + error);
